df1f6e4c495f911d21226f903cfd8ca69f9076c1
Daiane Dias <diasdaianedd@gmail.com>
2022-12-29 14:55:39 -0300

    Modificações e criação do metodo POST para adicionar uma lista de filmes

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    [StringLength(100)]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage ="O tamanho do gênero não pode exceder a 50 carateres.")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatória")]
+    [Range(70,600,ErrorMessage = "O filme deve durar entre 70 a 600.")]
+    public int Duracao { get; set; }
+
+}
