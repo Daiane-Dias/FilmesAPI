070960c0803648a3392c7ae08a2548e20be7fd98
Daiane Dias <diasdaianedd@gmail.com>
2022-12-30 08:44:00 -0300

    melhorandos os metodos

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -9,14 +9,14 @@ public class FilmeController : ControllerBase
     private static List<Filme> filmes = new List<Filme>();
     private static int id = 0;
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
 
         filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
-        Console.WriteLine(filme.Genero);
+        return CreatedAtAction(nameof(RecuperaFilmePorId), 
+            new { id = filme.Id}, 
+            filme);
     }
 
     [HttpGet]
@@ -25,8 +25,10 @@ public class FilmeController : ControllerBase
         return filmes.Skip(skip).Take(take);
     }
     [HttpGet("{id}")]
-    public Filme? RecuperaFilmePorId(int id)
+    public IActionResult RecuperaFilmePorId(int id)
     {
-       return filmes.FirstOrDefault(filme => filme.Id == id);
+       var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
